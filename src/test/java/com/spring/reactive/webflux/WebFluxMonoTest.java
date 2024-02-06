5283e4cde3fcafc1c80fd7eb66ab165d417071bd
rathnavelvija <rathnavelvija@gmail.com>
2024-02-06 20:32:23 +0530

    Initial Commit

--- src/test/java/com/spring/reactive/webflux/WebFluxMonoTest.java
+++ src/test/java/com/spring/reactive/webflux/WebFluxMonoTest.java
@@ -2,15 +2,41 @@ package com.spring.reactive.webflux;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @SpringBootTest
 public class WebFluxMonoTest {
 
     @Test
-    public void Mono(){
+    public void testMono(){
        // Mono.just(1);
-        Mono<String> stringMono = Mono.just("2").log();
-        stringMono.subscribe();
+        Mono<?> stringMono = Mono.just("Vija")
+                .then(Mono.error(new RuntimeException("Internal server Error")))
+                .doOnError( ex -> {
+                    System.out.println("Error occured"+ ex.getMessage());
+                })
+                .onErrorReturn("Fallback  value") //this will work above do on error wont work
+                .log();
+        stringMono.subscribe( e-> {
+            System.out.println(e);
+        });
+    }
+
+    @Test
+    public void testFlux(){
+        Flux<?> stringFlux =
+                Flux.just("Spring","Reactive","Programming","Web")
+                        .concatWithValues("Flux")
+                        .concatWith(Flux.error(new RuntimeException("Internal Server Error")))
+                        .concatWithValues("This won't be concated as after error this subscribe ")
+                        .log();
+        stringFlux.subscribe(e -> {
+            System.out.println(e);
+        }, ex -> {
+                    System.out.println(ex.getMessage());
+                }
+        );
+
     }
 }
